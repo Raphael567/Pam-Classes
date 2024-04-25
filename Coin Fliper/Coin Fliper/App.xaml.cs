8b7cad95014f2d1b700d26e1f439b3a106a8842c
Raphael567 <raphaelcanuto464@gmail.com>
2024-04-25 12:38:59 -0300

    Chore

---
+++ Coin Fliper/Coin Fliper/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Coin_Fliper
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Coin Fliper/Coin Fliper/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Coin_Fliper
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
